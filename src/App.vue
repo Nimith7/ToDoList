9bfed06f4952bdad6da8446ff463544ddaa4d09f
Nicolas Calvi <nicolascalvi.35@gmail.com>
2022-05-01 21:34:14 +0200

    update css

--- src/App.vue
+++ src/App.vue
@@ -79,33 +79,4 @@ header {
 ul {
   list-style-type: none;
 }
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-}
 </style>
